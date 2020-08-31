5762be868e938c61e3b2138f27e2c228b67be869
Abdul Hannan <abdulhannan5072.ah@gmail.com>
2020-08-31 15:43:06 +0500

    socket fix

--- client/src/chat/Chat.js
+++ client/src/chat/Chat.js
@@ -10,7 +10,7 @@ import { connect } from "react-redux";
 import User from "./User";
 
 let socket;
-const ENDPOINT = "localhost:4000";
+const ENDPOINT = process.env.PORT || "localhost:4000";
 
 class Chat extends React.Component {
   constructor(props) {
